a6d4bef741db5ffd40f1e3d65e3eda59ab98fb7d
maks <mamonov.maksim.20003@gmail.com>
2023-05-08 17:55:35 +0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Дима", "Коля", "Вика", "Аня");
+        List<String> editedNames = names.stream()
+                .map(name -> name.substring(1))
+                .collect(Collectors.toList());
+        System.out.println(editedNames);
+
+    }
+}
